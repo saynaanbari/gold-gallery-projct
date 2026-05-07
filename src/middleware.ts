a8e0951d3a7f93c118541c258982e0d763ef4c47
sayna <saenaanbari@gmail.com>
2026-05-07 14:38:26 +0330

    refactor:replace old middleware with updated middleware

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,20 +5,19 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
   const role = request.cookies.get("role")?.value;
   const { pathname } = request.nextUrl;
+  if (pathname === "/my-secret-panel-15j30k" && token) {
+    return NextResponse.redirect(new URL("/dashboard", request.url));
+  }
   if (pathname.startsWith("/dashboard")) {
     if (!token) {
       return NextResponse.rewrite(new URL("/not-found", request.url));
     }
     if (role !== "admin") {
-      return new NextResponse("شما اجازه دسترسی به این صفحه را ندارید.", {
-        status: 403,
-      });
+      return NextResponse.rewrite(new URL("/not-found", request.url));
     }
-    return NextResponse.next();
   }
   return NextResponse.next();
 }
-
 export const config = {
-  matcher: ["/dashboard/:path*"],
+  matcher: ["/dashboard/:path*", "/my-secret-panel-15j30k"],
 };
