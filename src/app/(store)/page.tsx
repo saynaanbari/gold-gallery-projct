37750fdc45a7d8456a26ac3c4e5efb142a8aacd8
sayna <saenaanbari@gmail.com>
2026-05-11 15:11:27 +0330

    feat: add hero section swiper to home page

--- src/app/(store)/page.tsx
+++ src/app/(store)/page.tsx
@@ -1,3 +1,9 @@
+import HeroSection from "@/components/store/home/components/hero-section";
+
 export default function Home() {
-  return <div>صفحه هوم</div>;
+  return (
+    <div className="container mx-auto">
+      <HeroSection />
+    </div>
+  );
 }
---
+++ src/components/store/home/components/hero-section.tsx
@@ -0,0 +1,88 @@
+"use client";
+import Image from "next/image";
+import heroSection from "@/assets/images/slider-01.jpg";
+import heroSection2 from "@/assets/images/slider-02.jpg";
+import Link from "next/link";
+import { Autoplay } from "swiper/modules";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.css";
+
+export default function HeroSection() {
+  return (
+    <div className="relative w-full h-80 md:h-130 lg:h-115 md:mt-8 lg:mt-6 mb-20">
+      <Swiper
+        modules={[Autoplay]}
+        spaceBetween={0}
+        slidesPerView={1}
+        loop={true}
+        speed={1000}
+        autoplay={{
+          delay: 6000,
+          disableOnInteraction: false,
+        }}
+        style={{
+          width: "100%",
+          height: "100%",
+        }}
+      >
+        <SwiperSlide
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            src={heroSection}
+            alt=""
+            style={{
+              display: "block",
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              cursor: "pointer",
+            }}
+          />
+        </SwiperSlide>
+        <SwiperSlide
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            src={heroSection2}
+            alt=""
+            style={{
+              display: "block",
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              cursor: "pointer",
+            }}
+          />
+        </SwiperSlide>
+        {/* عکس hero3 استفاده نشده، اگر لازم نیست حذفش کن */}
+        {/* <SwiperSlide
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <Image
+            src={hero3}
+            alt="Slide 3"
+            style={{
+              display: "block",
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+            }}
+          />
+        </SwiperSlide> */}
+      </Swiper>
+    </div>
+  );
+}
