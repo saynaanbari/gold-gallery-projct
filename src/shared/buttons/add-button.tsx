65e4afd5f63c5c16ebb91984552407a854ee6d0d
sayna <saenaanbari@gmail.com>
2026-05-18 13:09:28 +0330

    style: little style change

--- src/shared/buttons/add-button.tsx
+++ src/shared/buttons/add-button.tsx
@@ -9,8 +9,8 @@ export default function AddButton({
   title: string;
 }) {
   return (
-    <button className="flex items-center gap-2 bg-[#a9bd86] px-3 py-2 rounded-lg">
-      <Image src={img} alt={""} width={20} height={20} />
+    <button className="flex items-center gap-1 bg-[#a9bd86] hover:bg-[#9daf7d] px-3 py-2 rounded-lg">
+      <Image src={img} alt={""} width={20} height={20} className="cursor-pointer"/>
       <div className={className}>{title}</div>
     </button>
   );
