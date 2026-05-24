e90743868eb682ba34c79d218c6969f4e0630696
sayna <saenaanbari@gmail.com>
2026-05-24 14:26:06 +0330

    feat: add auth page with status-based login/register switching

--- src/app/(store)/auth/page.tsx
+++ src/app/(store)/auth/page.tsx
@@ -1,3 +1,20 @@
+"use client";
+import Login from "@/components/store/auth/components/login";
+import Register from "@/components/store/auth/components/register";
+import authImg from "@/assets/images/auth-background1.jpg";
+import { useSearchParams } from "next/navigation";
+
 export default function Auth() {
-  return <div>صفحه احراز هویت</div>;
+  const searchParams = useSearchParams();
+  const status = searchParams.get("status") || "login";
+  return (
+    <div
+      className="w-full h-screen absolute top-0 left-0 z-80 flex flex-col items-center md:justify-center gap-5 bg-cover bg-center bg-no-repeat"
+      style={{ backgroundImage: `url(${authImg.src})` }}
+    >
+      <div className="py-20">
+        {status === "login" ? <Login /> : <Register />}
+      </div>
+    </div>
+  );
 }
