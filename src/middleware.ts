0860da5a890ec318d84bffbd2a50b17a5b599a92
sayna <saenaanbari@gmail.com>
2026-05-24 14:03:57 +0330

    feat: add authentication redirect logic to auth route

--- src/middleware.ts
+++ src/middleware.ts
@@ -5,6 +5,10 @@ export function middleware(request: NextRequest) {
   const token = request.cookies.get("token")?.value;
   const role = request.cookies.get("role")?.value;
   const { pathname } = request.nextUrl;
+
+  if (pathname === "/auth" && token) {
+    return NextResponse.redirect(new URL("/", request.url));
+  }
   if (pathname === "/my-secret-panel-15j30k" && token && role === "admin") {
     return NextResponse.redirect(new URL("/dashboard", request.url));
   }
@@ -19,5 +23,5 @@ export function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 export const config = {
-  matcher: ["/dashboard/:path*", "/my-secret-panel-15j30k"],
+  matcher: ["/dashboard/:path*", "/my-secret-panel-15j30k", "/auth"],
 };
