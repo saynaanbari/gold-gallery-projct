6cbb05449b8728588fbcbc5d96bff48fda8a7528
sayna <saenaanbari@gmail.com>
2026-05-28 11:25:06 +0330

    feat: add Redux store and wrap layout with Provider

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import GlobalToaster from "@/shared/toast/toast";
 import { AuthProvider } from "@/context/authContext";
+import { Providers } from "@/redux/providers";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,10 +32,12 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        <AuthProvider>
-          <GlobalToaster />
-          {children}
-        </AuthProvider>
+        <Providers>
+          <AuthProvider>
+            <GlobalToaster />
+            {children}
+          </AuthProvider>
+        </Providers>
       </body>
     </html>
   );
